42ca091a6de5d64116af2a8375507efcf85d728d
kashmirabhingare <kashmirabhingare@gmail.com>
2025-05-01 14:18:53 +0530

    Create movie_model.dart

---
+++ movie_model.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+class MovieModel{
+  String? movie_title;
+  String? movie_genres;
+  String? movie_poster_url;
+  double? rating;
+
+  MovieModel(this.movie_title,this.movie_genres,this.rating,this.movie_poster_url);
+
+
+
+}
